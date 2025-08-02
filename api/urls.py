026a7af2483116e1c74fcd1a9c5efe1c9c4aab3b
well-versed <understandingroom@gmail.com>
2025-08-02 17:25:24 +0300

    initial commit of an api bookviewlist

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+router= DefaultRouter()
+router.register(r"books_all",BookViewSet)
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.generics import ListAPIView
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(ListAPIView):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
