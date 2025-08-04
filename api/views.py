e6b7f6858cc326b09854aa30769aa37e890f5214
well-versed <understandingroom@gmail.com>
2025-08-04 05:57:19 +0300

    added new editions

--- api/views.py
+++ api/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated  # Add this if missing
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Add this line
