d8bb4b321acf1ebc6396c6ee782cd5062981ba04
well-versed <understandingroom@gmail.com>
2025-08-10 09:52:26 +0300

    changes on the views and urls file

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-<<<<<<< HEAD
 from .models import Author, Book
 
 # =======================
@@ -40,11 +39,9 @@ class AuthorSerializer(serializers.ModelSerializer):
 
     # NOTE: The relationship is handled using the 'related_name' in Book model's ForeignKey.
     # This lets DRF automatically fetch the author's books for serialization.
-=======
 from .models import Book  
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('title', "author") 
->>>>>>> e6b7f6858cc326b09854aa30769aa37e890f5214
--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,25 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import ( 
+    BookViewSet,BookListCreateView, BookRetrieveUpdateDestroyView,
+     BookCreateView, BookUpdateView, BookDeleteView
+         )
 
+
+#router viewsets
 router= DefaultRouter()
 router.register(r"books_all",BookViewSet)
 
 urlpatterns = [
+    #viewset routes
     path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    
+    #generic view routes
+    path('books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-detail'),
+     path('books-create/', BookCreateView.as_view(), name='book-create'),
+    path('books-update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books-delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
 ]
